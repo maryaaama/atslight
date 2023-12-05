d22feb3599a1d5ae3ee1115e601ebba177e78ce3
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2023-12-05 15:39:31 +0330

    add-some-queries

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -27,3 +27,62 @@ export const GET_CANDIDATE = gql`
     }
   }
 `;
+export const GET_JOBS = gql`
+ query Jobs(
+  $filter: JobFilter
+  $orderBy: [JobsOrderBy!]
+  $first: Int
+  $offset: Int
+) {
+  jobs(filter: $filter, orderBy: $orderBy, first: $first, offset: $offset) {
+    nodes {
+      nodeId
+      id
+      translations {
+        nodes {
+          lang
+          nodeId
+          title
+        }
+      }
+      status
+      department {
+        nodeId
+        translations {
+          nodes {
+            nodeId
+            lang
+            title
+          }
+        }
+      }
+      canManageCandidates
+    }
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+    }
+  }
+}`;
+ export const GET_USER=gql`
+ query Users(
+  $first: Int
+  $offset: Int
+  $orderBy: [UsersOrderBy!]
+  $filter: UserFilter
+) {
+  users(first: $first, offset: $offset, orderBy: $orderBy, filter: $filter) {
+    nodes {
+      id
+      nodeId
+      translations {
+        nodes {
+          nodeId
+          lang
+          fullname
+          initials
+        }
+      }
+    }
+  }
+}`;
