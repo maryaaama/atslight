9d32656da36af1e638f3660051cb0ed7088967b3
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2023-12-05 16:09:22 +0330

    add-type-options-file

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import {JobField } from './graphql/generated/graphql';
+
+export type FileWithId = {
+  file: File;
+  id: string;
+};
+
+export type UploadedFile = {
+  name: string;
+  type: string;
+  externalId: string;
+  localId: string;
+};
+
+export interface Field {
+  type: JobField;
+  position: number;
+  required: boolean;
+}
+
