edd818ae0195b71993102688f8ee3cb369b3a228
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-01-16 13:35:13 +0330

    feat-fix

--- src/routes/jobs/page.tsx
+++ src/routes/jobs/page.tsx
@@ -5,32 +5,28 @@ import NavBar from "../../components/navBar/navBar";
 import JobSkeleton from "../../components/skeleton/Job";
 
 export default function Jobs() {
-  const { data, loading, error } = useJobsQuery({});
-
-  if (loading) {
-    return <JobSkeleton />;
-  }
+  const { data, loading, error } = useJobsQuery();
 
   if (error) {
     return <div>Error fetching data</div>;
   }
 
   const jobs = data?.jobs?.nodes || [];
-  console.log(jobs);
+  console.log(data);
   return (
+    loading ? <JobSkeleton/>:
     <main className="md:h-screen">
       <NavBar name={"آگهی های شغلی"} />
       <div className="sm:h-11/12 md:p-4 md:h-[85%] md:overflow-auto max-sm:w-screen max-w-xl mx-auto sm:border sm:mt-8 sm:rounded-lg sm:items-center sm:shadow-lg">
-        <ul>
+        <div>
           {jobs.map((job) => (
-            <li key={job.id}>
+            <div key={job.id}>
               {job.translations.nodes.length > 0 ? job.translations.nodes[0].title : ""}
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </main>
     
   );
-  
 }
