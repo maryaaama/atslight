f94352222a3dc8f196530c00b1a86930be71b086
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-01-21 13:07:37 +0330

    fix-skeleton-and-jobs-modal

--- src/components/skeleton/candidateCard.tsx
+++ src/components/skeleton/candidateCard.tsx
@@ -1,14 +1,15 @@
 const CandidateCardSkeleton = () => {
   return (
     <span
-      className="flex m-1 rounded-md items-center animate-pulse bg-gray-200"
+      className="flex w-auto m-1 rounded-md border justify-between items-center animate-pulse bg-gray-200"
+
     >
       <div className="rounded-xl m-1.5 border w-[90px] h-[90px] bg-gray-300"></div>
       <div className="w-[46%] m-1 pb-4">
         <div className="h-4 bg-gray-300 rounded w-3/4"></div>
         <div className="h-2 bg-gray-300 rounded w-5/6 mt-3"></div>
       </div>
-      <div className="w-[28%] h-24 flex flex-col justify-between mx-1 my-0 py-2 rounded-xl bg-gray-300">
+      <div className="w-[26%] h-[90px] md:w-[20%] m-1 border flex flex-col justify-between my-0 py-2 rounded-xl bg-gray-300">
         <div className="h-4 w-1/2 bg-gray-200 rounded text-center m-auto py-2"></div>
         <div className="h-2 w-1/2 bg-gray-200 rounded text-center m-auto p-0.5"></div>
       </div>
--- src/components/skeleton/candidates.tsx
+++ src/components/skeleton/candidates.tsx
@@ -4,10 +4,10 @@ import CandidateCardSkeleton from "./candidateCard";
 
   return (
     <main className="md:h-screen">
-      <div className="sm:h-11/12 md:p-4 md:h-[85%] md:overflow-auto max-w-xl mx-auto sm:border sm:mt-8 sm:rounded-lg sm:items-center">
-        <div className="w-2/3 h-10 mx-auto my-2 py-1.5 border rounded-lg bg-gray-300 animate-pulse flex items-center">
+      <div className="sm:h-11/12 md:h-[85%] md:overflow-auto max-sm:w-screen max-w-xl mx-auto sm:border sm:rounded-lg sm:items-center sm:shadow-lg">
+        {/* <div className="w-2/3 h-10 mx-auto my-2 py-1.5 border rounded-lg bg-gray-300 animate-pulse flex items-center">
           <div className="h-3 mx-auto bg-gray-100 rounded w-3/4"></div>
-        </div>
+        </div> */}
         {cardCount.map((_, index) => (
           <CandidateCardSkeleton key={index} />
         ))}
