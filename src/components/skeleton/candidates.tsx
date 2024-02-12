750fafac98399950966ee2438da3cc73d9246e07
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-02-12 17:07:15 +0330

    fix-loading

--- src/components/skeleton/candidates.tsx
+++ src/components/skeleton/candidates.tsx
@@ -1,13 +1,15 @@
+import NavBar from "../navBar/navBar";
 import CandidateCardSkeleton from "./candidateCard";
 const CandidatesSkeleton = () => {
   const cardCount = Array.from({ length: 7 });
 
   return (
     <main className="md:h-screen">
+      <NavBar name="" />
       <div className="sm:h-11/12 md:h-[85%] md:overflow-auto max-sm:w-screen max-w-xl mx-auto sm:border sm:rounded-lg sm:items-center sm:shadow-lg">
-        {/* <div className="w-2/3 h-10 mx-auto my-2 py-1.5 border rounded-lg bg-gray-300 animate-pulse flex items-center">
+        <div className="w-2/3 h-10 mx-auto my-2 py-1.5 border rounded-lg bg-gray-300 animate-pulse flex items-center">
           <div className="h-3 mx-auto bg-gray-100 rounded w-3/4"></div>
-        </div> */}
+        </div>
         {cardCount.map((_, index) => (
           <CandidateCardSkeleton key={index} />
         ))}
--- src/routes/candidates/page.tsx
+++ src/routes/candidates/page.tsx
@@ -43,6 +43,11 @@ export default function Candidates() {
             .nodes[0]?.title || "loading..."
         }
       />
+      <div className="w-full border-b">
+        <div className="w-2/3 mx-auto my-2 py-1.5 text-center border rounded-lg bg-gray-100 hover:bg-opacity-80 shadow-sm hover:shadow">
+          <button>ویرایش آگهی استخدام</button>
+        </div>
+      </div>
       <div className="sm:h-11/12 md:p-4 md:h-[85%] md:overflow-auto max-sm:w-screen max-w-xl mx-auto sm:border sm:mt-8 sm:rounded-lg sm:items-center sm:shadow-lg">
         {candidatesData?.candidates?.nodes.length === 0 ? (
           <EmptyState />
